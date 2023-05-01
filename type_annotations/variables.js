cab0e72165bd57c568e5e9e16b7f5f8cfc90184f
Michael Appiah <michaelkwameappiahamankwah@gmail.com>
2023-05-01 00:49:03 +0000

    Basic type annotations

---
+++ type_annotations/variables.js
@@ -0,0 +1,9 @@
+// Declaring a string variable
+var movieTitle = 'Django Unchained';
+movieTitle = 'Inglorious Basterds'; // reassigning movieTitle to a new string value
+// Declaring a number variable
+var movieYear = 2012;
+movieYear = 2009; // reassigning movieYear to a new number value
+// Declaring a boolean variable
+var gameOver = true;
+gameOver = false;
---
+++ type_annotations/variables.ts
@@ -0,0 +1,11 @@
+// Declaring a string variable
+let movieTitle: string = 'Django Unchained';
+movieTitle = 'Inglorious Basterds'; // reassigning movieTitle to a new string value
+
+// Declaring a number variable
+let movieYear: number = 2012;
+movieYear = 2009; // reassigning movieYear to a new number value
+
+// Declaring a boolean variable
+let gameOver: boolean = true;
+gameOver = false;
